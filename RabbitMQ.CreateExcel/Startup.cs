6950fbf9709e0f88b62160321daa70ad3b538760
Buğrahan <bsaygici@hotmail.com>
2022-02-05 00:45:23 +0300

    Added RabbitMQClientService to the project

--- RabbitMQ.CreateExcel/Startup.cs
+++ RabbitMQ.CreateExcel/Startup.cs
@@ -6,7 +6,9 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using RabbitMQ.Client;
 using RabbitMQ.CreateExcel.Models;
+using RabbitMQ.CreateExcel.Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -26,6 +28,11 @@ namespace RabbitMQ.CreateExcel
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSingleton(sp => new ConnectionFactory()
+            {
+                Uri = new Uri(Configuration.GetConnectionString("RabbitMQ")),DispatchConsumersAsync=true
+            });
+            services.AddSingleton<RabbitMQClientService>();
             services.AddDbContext<AppDbContext>(options =>
             {
                 options.UseSqlServer(Configuration.GetConnectionString("SqlServer"));
